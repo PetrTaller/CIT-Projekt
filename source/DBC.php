0f33cc6d82d5105ba3f96d7acdf9af30a001700e
PetrTaller <127184208+PetrTaller@users.noreply.github.com>
2024-11-03 19:15:27 +0100

    Update DBC.php

--- source/DBC.php
+++ source/DBC.php
@@ -50,7 +50,8 @@ public static function insertUser($username, $password)
     $connection = self::getConnection();
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
     $profile_id = 1;
-    $query = "INSERT INTO users (username, password,profile_id) VALUES ('$username','$hashedPassword','$profile_id')";
+    $is_admin = 0;
+    $query = "INSERT INTO users (username, password,profile_id,is_admin) VALUES ('$username','$hashedPassword','$profile_id','$is_admin')";
     return mysqli_query($connection, $query);
 }
 
@@ -80,19 +81,71 @@ public static function getProfileId($username)
     return $row['profile_id'];
 }
 
-public static function insertQuote($author, $content)
+public static function insertBlogPost($content,$author,$visibility)
 {
     $connection = self::getConnection();
-    $query = "INSERT INTO quotes (author, content) VALUES ('$author','$content')";
+    $query = "INSERT INTO blog_posts (content, author, visibility) VALUES ('$content','$author','$visibility')";
     return mysqli_query($connection, $query);
 }
 
-public static function getQuote($id)
+public static function getBlogPost($id)
 {
     $connection = self::getConnection();
-    $query = "SELECT * FROM quotes WHERE id='$id'";
+    $query = "SELECT * FROM blog_posts WHERE id='$id'";
     $result = mysqli_query($connection, $query);
     return mysqli_fetch_assoc($result);
+}
+
+public static function getBlogPosts($username = null) {
+    $connection = self::getConnection();
+    if ($username) {
+        if (self::isAdmin($username)) {
+            $query = "SELECT * FROM blog_posts";
+        } else {
+            $query = "SELECT * FROM blog_posts WHERE author='$username' OR visibility='public'";
+        }
+    } else {
+        $query = "SELECT * FROM blog_posts WHERE visibility='public'";
+    }
+    $result = mysqli_query($connection, $query);
+    return $result;
+}
+
+public static function deleteBlogPost($id) {
+    $connection = self::getConnection();
+    $query = "DELETE FROM blog_posts WHERE id='$id'";
+    return mysqli_query($connection, $query);
+}
+
+public static function updateBlogPost($id, $data) {
+    $connection = self::getConnection();
+    $updates = [];
+    if (isset($data['content'])) {
+        $content = mysqli_real_escape_string($connection, $data['content']);
+        $updates[] = "content='$content'";
+    }
+    if (isset($data['visibility'])) {
+        $visibility = mysqli_real_escape_string($connection, $data['visibility']);
+        $updates[] = "visibility='$visibility'";
+    }
+    if (!empty($updates)) {
+        $query = "UPDATE blog_posts SET " . implode(", ", $updates) . " WHERE id='$id'";
+        return mysqli_query($connection, $query);
+    }
+    return false;
+}
+
+public static function isAdmin($username) {
+    $connection = self::getConnection();
+    $username = mysqli_real_escape_string($connection, $username);
+    $query = "SELECT is_admin FROM users WHERE username='$username'";
+    $result = mysqli_query($connection, $query);
+    $row = mysqli_fetch_assoc($result);
+    if($result){
+    return $row['is_admin'] == 1;
+    }else{
+    return FALSE;
+    }
 }
 
 }
